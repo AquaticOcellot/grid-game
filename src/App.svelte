ff3775a623956cd8f6a76ab2e265f8b2e2113a50
Aquat <aquaticocellot>
2024-01-30 13:26:19 +0300

    restructure texture loading

--- src/App.svelte
+++ src/App.svelte
@@ -41,7 +41,7 @@
     function setupAssets() {
         PIXI.Assets.add({
             alias: "metal",
-            src: "/assets/metal.png"
+            src: "/src/assets/metal.png"
         });
     }
 
