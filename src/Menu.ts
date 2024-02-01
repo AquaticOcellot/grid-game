6f2e2b48c0cf8b29f46da8792f689b0c261fd340
Aquat <aquaticocellot>
2024-02-01 12:43:54 +0300

    restructuring

--- src/Menu.ts
+++ src/Menu.ts
@@ -2,24 +2,31 @@ import * as PIXI from "pixi.js"
 import Slider from "./Slider";
 import Button from "./Button";
 import SwitchButton from "./SwitchButton";
+
+const margin = 10
+const widthSliderDimensions = [600, 50]
+const heightSliderDimensions = [800, 50]
+
 export default ((coordinates: number[], options: any) : PIXI.Container => {
-    let container = new PIXI.Container()
-    container.position.set(coordinates[0], coordinates[1])
+    let widthSlider = Slider([0, 0],
+        widthSliderDimensions, [1, 100], options.gridWidth)
+    let heightSlider = Slider([widthSlider.x, widthSlider.height + margin],
+        heightSliderDimensions, [1, 50], options.gridHeight)
+
+    let generateButton = Button("Generate", options.generateFunction,
+        [heightSlider.x, heightSlider.y + heightSlider.height + margin])
+    let manualUpdate = Button("Manual update", options.manualUpdateFunction,
+        [generateButton.x + generateButton.width + margin, heightSlider.y + heightSlider.height + margin])
+    let autoUpdate = SwitchButton("Auto update", options.autoUpdate,
+        [manualUpdate.x + manualUpdate.width + margin, heightSlider.y + heightSlider.height + margin])
 
     let background = new PIXI.Graphics()
-        .beginFill(0x111111).drawRect(0, 0, 100, 100)
 
-    let widthSlider = Slider([0, 0], [600, 50], [1, 100], options.gridWidth)
-    let heightSlider = Slider([0, 50], [800, 50], [1, 50], options.gridHeight)
-
-    let generateButton = Button("Generate", options.generateFunction,[0, 100])
-    let manualUpdate = Button("Manual update", options.manualUpdateFunction,
-        [generateButton.x + generateButton.width + 10, generateButton.y])
-    let autoUpdate = SwitchButton("Auto update", options.autoUpdate,
-        [manualUpdate.x + manualUpdate.width + 10, manualUpdate.y])
-
+    let container = new PIXI.Container()
+    container.position.set(coordinates[0], coordinates[1])
     container.addChild(background, widthSlider, heightSlider, generateButton, manualUpdate, autoUpdate)
 
+    background.beginFill(0x111111).drawRect(0, 0, container.width, container.height )
 
 
     return container
